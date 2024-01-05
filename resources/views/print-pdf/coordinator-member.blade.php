338f9d9065875f4381cc1015b75e8df852035d0e
Sunardi <71060025+naldy-dy@users.noreply.github.com>
2024-01-06 02:15:56 +0700

    update sikit

--- resources/views/print-pdf/coordinator-member.blade.php
+++ resources/views/print-pdf/coordinator-member.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<title>DATA {{strtoupper($voter->name)}} {{date('d-m-Y')}}</title>
 	 <link href="{{ asset('assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 
 </head>
