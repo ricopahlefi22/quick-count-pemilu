953fdc92b21fa0a34a7bdd06d9a10e95987177fd
Sunardi <71060025+naldy-dy@users.noreply.github.com>
2024-01-06 02:14:20 +0700

    update cetak

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -4,13 +4,30 @@ namespace App\Http\Controllers;
 
 use Barryvdh\DomPDF\Facade\Pdf;
 
+use App\Models\District;
+use App\Models\Village;
+use App\Models\Voter;
+use App\Models\VotingPlace;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Crypt;
+use Yajra\DataTables\Facades\DataTables;
+
+
 class PrintController extends Controller
 {
-    function coordinatorMember()
-    {
-        $pdf = Pdf::loadView('print-pdf.coordinator-member')->setPaper('A4', 'landscape');
+    function coordinatorMember(Request $request){
 
-        // return $pdf->stream();
-        return view('print-pdf.coordinator-member');
-    }
+        // data
+     $data['voter'] = Voter::findOrFail(Crypt::decrypt($request->id));
+     $data['title'] = 'Detail Data ' . $data['voter']->name;
+     $data['districts'] = District::all();
+     $data['members'] = Voter::where('coordinator_id', $data['voter']->id)->orderBy('level', 'desc')->get();
+
+        // end data
+    // $pdf = Pdf::loadView('print-pdf.coordinator-member')->setPaper('A4', 'landscape');
+
+    return view('print-pdf.coordinator-member', $data);
+}
+
 }
